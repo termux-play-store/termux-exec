6074163f77f61e345b9972dca943d9b4cf40befd
Fredrik Fornwall <fredrik@fornwall.net>
2025-07-10 23:28:07 +0200

    Format

--- tests/exec-directory.c
+++ tests/exec-directory.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int, char **argv, char** env) {
+int main(int, char **argv, char **env) {
   execve(".", argv, env);
   printf("errno = %d\n", errno);
   return 0;
